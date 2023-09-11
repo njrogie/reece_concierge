7657bfcf55aecd68ad84f20302662528cd77d677
njrogie <njrogie@gmail.com>
2023-09-10 19:44:09 -0500

    Added a few commands to the bot.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,83 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use dotenv::dotenv;
+
+use serenity::async_trait;
+use serenity::model::prelude::command;
+use serenity::prelude::*;
+use serenity::utils::MessageBuilder;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
+
+#[group]
+#[commands(ping)]
+#[commands(count)]
+#[commands(setchannel)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+    Ok(())
+}
+
+#[command]
+async fn count(ctx: &Context, msg: &Message) -> CommandResult {
+    let response = MessageBuilder::new()
+        .mention(&msg.author)
+        .push(" has 0 messages in this server (I haven't started counting them yet, numb nuts)")
+        .build();
+    msg.reply(ctx, response).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn setchannel(ctx: &Context, msg: &Message) -> CommandResult {
+    //let channel = msg.channel_id.to_string();
+
+    //save_channelid(channel);
+
+    let channel = match msg.channel_id.to_channel(&ctx).await {
+        Ok(channel) => channel,
+        Err(why) => {
+            println!("Error getting channel: {:?}", why);
+            return Ok(());
+        },
+    };
+
+    let response = MessageBuilder::new()
+        .push("Concierge has set up his desk in ")
+        .mention(&channel)
+        .build();
+
+    msg.reply(ctx, response).await?;
+    Ok(())
 }
